f01541a1589aa0493fe10fb76816fdd35e5ffb3b
Jolinda-hub <60364539+Jolinda-hub@users.noreply.github.com>
2022-09-21 20:38:44 +0200

    Update environment.ts

--- udagram-frontend/src/environments/environment.ts
+++ udagram-frontend/src/environments/environment.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   appName: 'Udagram',
-  apiHost: 'http://localhost:8080/api/v0'
+  apiHost: 'http://a0d3d4431920f4585b2d0b973704f27d-1469447180.us-east-1.elb.amazonaws.com:8080/api/v0'
 };
 
 /*
